377d05d0e0e42f3356dd65e79f2825dc38f372aa
Charneaka Lewis <clewis20@ggc.edu>
2023-01-23 21:03:41 -0500

    Added methods to load categories to the board

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 initBoard();
 initCatRow();
 
+document.querySelector('button').addEventListener('click', buildCategories)
+
 function initCatRow() {
     let catRow = document.getElementById('category-row')
 
@@ -32,6 +34,64 @@ function initBoard() {
     }
 }
 
-function getClue(){
+function randInt() {
+    return Math.floor(Math.random() * (18418) + 1)
+}
+
+let catArr = []
+
+function buildCategories() {
+    const fetchReq1 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+    
+    const fetchReq2 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq3 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq4 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq5 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq6 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const allData = Promise.all([fetchReq1, fetchReq2, fetchReq3, fetchReq4, fetchReq5, fetchReq6])
+
+    allData.then((res) => {
+        console.log(res)
+        catArr = res
+        setCategories(catArr)
+    })
+}
+
+//FUNCTION TO LOAD THE CATEGORIES TO THE BOARD
+function setCategories(catArr) {
+    let element = document.getElementById('category-row')
+    let children = element.children
+    for (let i = 0; i < children.length; i++){
+        children[i].innerHTML = catArr[i].title
+    }
+}
+
+function getClue(event) {
+    let child = event.currentTarget
+    child.classList.add('clicked-box')
+    let boxValue = child.innerHTML.slice(1)
+    let parent = child.parentNode
+    let index = Array.prototype.findIndex.call(parent.children, (c) => c === child)
+}
+
+
+function getClue() {
     console.log('Have a nive day')
 }
